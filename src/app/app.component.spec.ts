11f724c4f7d304c9b7fa49bd1465157bab5845f6
Joster422 <Joster422@gmail.com>
2022-11-13 22:11:31 -0500

    feat: update ng

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,5 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import { AppModule } from './app.module';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -8,9 +7,6 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
-      imports: [
-        AppModule
-      ]
     }).compileComponents();
   });
 
@@ -18,5 +14,18 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'tic-tac-toe'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('tic-tac-toe');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.content span')?.textContent).toContain('tic-tac-toe app is running!');
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { ServiceWorkerModule } from '@angular/service-worker';
 
-import { FormControlModule } from '@joster-dev/form-control';
+import { ChaosControlModule } from '@joster-dev/chaos-control';
 import { IconModule } from '@joster-dev/icon';
 
 import { AppComponent } from './app.component';
-import { environment } from '../environments/environment';
 import { GameComponent } from './game/game.component';
 import { CellComponent } from './game/cell/cell.component';
 import { FormComponent } from './game/form/form.component';
@@ -22,12 +20,9 @@ import { FormComponent } from './game/form/form.component';
   ],
   imports: [
     BrowserModule,
-    FormControlModule,
+    ChaosControlModule,
     IconModule,
     FormsModule,
-    ServiceWorkerModule.register('ngsw-worker.js', {
-      enabled: environment.production
-    })
   ],
   providers: [],
   bootstrap: [AppComponent]
