4a810b9e9a9fd596666978764beb8a3aaf503b13
Joster422 <Joster422@gmail.com>
2022-11-13 22:17:06 -0500

    feat

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 import { ChaosControlModule } from '@joster-dev/chaos-control';
 import { IconModule } from '@joster-dev/icon';
@@ -9,6 +10,7 @@ import { AppComponent } from './app.component';
 import { GameComponent } from './game/game.component';
 import { CellComponent } from './game/cell/cell.component';
 import { FormComponent } from './game/form/form.component';
+import { environment } from '../environments/environment';
 
 
 @NgModule({
@@ -23,6 +25,12 @@ import { FormComponent } from './game/form/form.component';
     ChaosControlModule,
     IconModule,
     FormsModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
